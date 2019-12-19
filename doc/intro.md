a358523239f9dfc6ab76935e41b785129654f07e
Rafael Leal <rafaelealdias@gmail.com>
2019-12-19 11:28:07 -0300

    lein new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kubernetes-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/kubernetes_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns kubernetes-api.core-test
+  (:require [clojure.test :refer :all]
+            [kubernetes-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
