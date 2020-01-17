b2ee9284cb3180a6e6d438be3148412a3b991cfe
Rafael Leal <rafaelealdias@gmail.com>
2020-01-17 14:33:29 -0300

    Adds swagger-from test

--- test/kubernetes_api/extensions/custom_resource_definition_test.clj
+++ test/kubernetes_api/extensions/custom_resource_definition_test.clj
@@ -1,5 +1,6 @@
 (ns kubernetes-api.extensions.custom-resource-definition-test
   (:require [clojure.test :refer :all]
+            [matcher-combinators.test]
             [kubernetes-api.extensions.custom-resource-definition :as crd]))
 
 (deftest new-route-name-test
@@ -41,3 +42,46 @@
     (is (= "WatchTektonDevV1alpha1TaskListForAllNamespaces"
            (crd/new-route-name "watch" "tekton.dev" "v1alpha1" nil "TaskList" {:all-namespaces true})))))
 
+(deftest swagger-from-test
+  (testing "it generated CustomResourceDefinition paths"
+    (is (match?
+          {:paths {"/apis/tekton.dev/v1alpha1/tasks"
+                   {:get {:operationId "ListTektonDevV1alpha1TaskForAllNamespaces"}}
+
+                   "/apis/tekton.dev/v1alpha1/namespaces/{namespace}/tasks"
+                   {:get    {:operationId "ListTektonDevV1alpha1NamespacedTask"}
+                    :post   {:operationId "CreateTektonDevV1alpha1NamespacedTask"}
+                    :delete {:operationId "DeleteTektonDevV1alpha1CollectionNamespacedTask"}}
+
+                   "/apis/tekton.dev/v1alpha1/namespaces/{namespace}/tasks/{name}"
+                   {:get    {:operationId "ReadTektonDevV1alpha1NamespacedTask"}
+                    :patch  {:operationId "PatchTektonDevV1alpha1NamespacedTask"}
+                    :put    {:operationId "ReplaceTektonDevV1alpha1NamespacedTask"}
+                    :delete {:operationId "DeleteTektonDevV1alpha1NamespacedTask"}}}}
+          (crd/swagger-from
+            {:api     "tekton.dev"
+             :version "v1alpha1"}
+            {:kind         "APIResourceList"
+             :groupVersion "tekton.dev/v1alpha1"
+             :resources    [{:name               "tasks",
+                             :singularName       "task",
+                             :namespaced         true,
+                             :kind               "Task",
+                             :verbs              ["delete" "deletecollection" "get" "list" "patch" "create" "update" "watch"],
+                             :categories         ["all" "tekton-pipelines"],
+                             :storageVersionHash "Vwu99D/K4xM="}]}
+            {:kind  "CustomResourceDefinitionList"
+             :items [{:spec {:group        "tekton.dev"
+                             :version      "v1alpha1"
+                             :names        {:singular   "task"
+                                            :plural     "tasks"
+                                            :kind       "Task"
+                                            :listKind   "TaskList"
+                                            :categories ["all" "tekton-pipelines"]}
+                             :scope        "Namespaced"
+                             :subresources {:status {}}
+                             :versions     [{:name    "v1alpha1"
+                                             :served  true
+                                             :storage true
+                                             :schema  {:openAPIV3Schema
+                                                       {:type "object"}}}]}}]})))))
