93090ba4eb4d55f79f2da994948e71f70d93c409
Mateus Dubiela Oliveira <mateus.dubiela@nubank.com.br>
2023-05-12 17:06:50 -0300

    raise-interceptor: Improve tests

--- test/kubernetes_api/interceptors/raise_test.clj
+++ test/kubernetes_api/interceptors/raise_test.clj
@@ -1,25 +1,28 @@
 (ns kubernetes-api.interceptors.raise-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is testing]]
             [kubernetes-api.interceptors.raise :as interceptors.raise]
-            [matcher-combinators.test]))
+            [matcher-combinators.test :refer [match? thrown-match?]]
+            [tripod.context :as tc]))
+
+(defn- run-interceptor [interceptor input]
+  (tc/execute 
+   (tc/enqueue input interceptor)))
 
 (deftest raise-test
-  (let [{:keys [leave]} (interceptors.raise/new {})]
+  (let [raise-interceptor (interceptors.raise/new {})]
     (testing "should raise the body to be the response on 2xx status"
-      (is (match?
-           {:response {:my :body}}
-           (leave {:response {:status 200
-                              :body   {:my :body}}}))))
+      (is (match? {:response {:my :body}}
+                  (run-interceptor raise-interceptor {:response {:status 200 :body {:my :body}}}))))
+    
     (testing "should have the request/response on metadata"
-      (is (match?
-           {:request  {:my :request}
-            :response {:status 200
-                       :body   {:my :body}}}
-           (meta (leave {:request  {:my :request}
-                         :response {:status 200
-                                    :body   {:my :body}}})))))
+      (is (match? {:request  {:my :request}
+                   :response {:status 200
+                              :body   {:my :body}}}
+                  (meta
+                   (run-interceptor raise-interceptor {:request  {:my :request}
+                                                       :response {:status 200
+                                                                  :body   {:my :body}}})))))
 
     (testing "should raise exception on 4XX responses"
-      (is (thrown-match?
-           {:type :unauthorized}
-           (leave {:response {:status 401}}))))))
+      (is (thrown-match? {:type :unauthorized}
+                         (run-interceptor raise-interceptor {:response {:status 401}}))))))
