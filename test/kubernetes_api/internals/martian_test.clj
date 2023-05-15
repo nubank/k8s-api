a9213578cdd206b9adfa278d315df53a8a9c6835
Mateus Dubiela Oliveira <mateus.dubiela@nubank.com.br>
2023-05-15 15:14:11 -0300

    raise-interceptor: Make internal raise exception comming from interceptors

---
+++ test/kubernetes_api/internals/martian_test.clj
@@ -0,0 +1,15 @@
+(ns kubernetes-api.internals.martian-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [kubernetes-api.internals.martian :as internals.martian]
+            [martian.core :as martian]
+            [matcher-combinators.test :refer [thrown-match?]]
+            [mockfn.macros :refer [providing]]) 
+  (:import [clojure.lang ExceptionInfo]))
+
+(deftest response-for
+  (testing "Throws exception from client"
+    (providing [(martian/response-for 'martian 'testing) 
+                (delay {:kubernetes-api.core/error (ex-info "Test error" {:type :error})})]
+               (is (thrown-match? ExceptionInfo 
+                                  {:type :error}
+                                  (internals.martian/response-for 'martian 'testing))))))
